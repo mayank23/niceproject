f7e6eba60cb54c4814dd417a21c2b8370d5fc0ec
Andrew Nelson <apnelson@godaddy.com>
2015-07-28 23:38:26 -0700

    location search bar now moves map

--- include/init.js
+++ include/init.js
@@ -1,3 +1,10 @@
+var mapLocation = undefined;
+var autocomplete = undefined;
+var STARTVIEW = 0;
+var MEDVIEW = 1;
+var ENDVIEW = 2;
+var currView = STARTVIEW;
+
 var showMapView = function() {
   console.log("showmapview is called");
   $('#searching-mid').attr('id','searching-small');
@@ -10,8 +17,17 @@ var showMapView = function() {
     .addClass("mapview");
 
   recalcSize();
-  $("#map-canvas").show().gmap3();
+  $("#map-canvas").show().gmap3({
+    map: {options: {zoom:12}}
+  });
+  if (mapLocation) {
+    $("#map-canvas").gmap3("get").setCenter(mapLocation);
+  }
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    $("#map-canvas").gmap3("get").setCenter(autocomplete.getPlace().geometry.location);
+  });
   $("#side-bar").tabs();
+  currView = ENDVIEW;
 };
 
 var recalcSize = function() {
@@ -26,7 +42,12 @@ var recalcSize = function() {
 $(document).ready(function() {
   var input = (document.getElementById('searching-large'));
   console.log(input);
-  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete = new google.maps.places.Autocomplete(input);
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    mapLocation = autocomplete.getPlace().geometry.location;
+    if (currView === STARTVIEW)
+      showFilterView();
+  });
   $("#go-to-mapview").click(showMapView);
   $("#sortable-filters").sortable();
   $("#sortable-filters").disableSelection();
@@ -34,12 +55,13 @@ $(document).ready(function() {
 });
 
 var showFilterView = function() {
-
   $('#searching-large').attr('id','searching-mid');
   $('#search-submit-large').attr('id','search-submit-mid');
   $('#searchbox-large').attr('id','searchbox-mid');
   $('#start').attr('id','mid');
   console.log("called or not");
+  currView = MEDVIEW;
   return false;
 };
+
 $(window).resize(recalcSize);
