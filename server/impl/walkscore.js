cbc6eb48d1cecf343b3dbfcb9d95ccf5ff99a276
mayank23 <jethva@purdue.edu>
2015-07-28 18:28:12 -0700

    added more endpoints, cross origin working

---
+++ server/impl/walkscore.js
@@ -0,0 +1,18 @@
+var request = require('request');
+
+module.exports = function(req, res, next){
+
+var address = req.query.address;
+var url = 'https://www.walkscore.com/auth/_pv/overview/' + address + '?d=current';
+
+request(url, function(error, response, body){
+  if(error){
+    console.log(error);
+  }else{
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:52096');
+    res.send(body);
+  }
+  return next();
+});
+
+}
